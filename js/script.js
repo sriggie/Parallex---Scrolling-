170329faffdf2767215359d7f2413084d5521e83
quandries <simonreagan76@gmail.com>
2023-07-21 01:21:38 +0300

    + js parralax scroll effects

--- js/script.js
+++ js/script.js
@@ -8,5 +8,9 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
 });
